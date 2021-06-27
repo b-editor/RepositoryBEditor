8113b7d0a41738787a181bf40adbd8d2a89920c1
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-06-27 19:37:31 +0900

    [add] Ability to create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    user = User.new(user_params)
+    @state = user.save
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :usertype)
   end
 end
