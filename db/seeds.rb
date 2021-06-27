df3b598d009b8819d7fd85d7c0141735fd39bca2
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-06-27 16:12:03 +0900

    created function of show users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {
+    name: 'admin',
+    email: 'admin@example.com',
+    password_digest: '$2a$10$HjQH2VBdguACJLyZHoVSs.yBZbwypqY3vUJGnxlWj94rmilWIuWzK',
+    usertype: 0
+  },
+  {
+    name: 'user',
+    email: 'user@example.com',
+    password_digest: '$2a$10$HjQH2VBdguACJLyZHoVSs.yBZbwypqY3vUJGnxlWj94rmilWIuWzK',
+    usertype: 1
+  },
+])
