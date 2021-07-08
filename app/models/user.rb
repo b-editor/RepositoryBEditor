7859ed0c27585b5dbcc5cf6520cac09bbe86dbf4
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-07-08 15:59:15 +0900

    [add] ユーザのidから情報を取得できる機能を追加 / ユーザー作成機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :packages
-  enum usertype: {developer: 0, user: 1}
+  enum usertype: { developer: 0, user: 1 }
 end
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.user @user
+json.user @use
