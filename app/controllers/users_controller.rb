945b9fea2bee4dcaba59ed28742ef2d46e02a6cb
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-07-27 15:09:03 +0900

    [add] add ability setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
   def index
     @users = User.all
   end
@@ -8,13 +10,31 @@ class UsersController < ApplicationController
   end
 
   def create
+    binding.pry
     user = User.new(user_params)
+    if user_ability_params == 1
+      user.role = 3
+    else
+      user.role = 2
+    end
     @state = user.save
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    if user == current_user
+      user.destroy
+    elsif can? :destroy, User
+      user.destroy
+    end
   end
 
   private
 
+  def user_ability_params
+    params.permit(:developer)
+  end
   def user_params
-    params.permit(:name, :email, :password, :usertype)
+    params.permit(:name, :email, :password)
   end
 end
