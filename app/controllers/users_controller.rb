02550e4090e22cb8729dc4887c59ad7adcff2f41
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-07-27 15:49:41 +0900

    [add] feature of destroying user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:create]
 
+  # 一覧表示
   def index
     @users = User.all
   end
-
+  # 一件表示
   def show
     @user = User.find(params[:id])
   end
-
+  # 更新
   def update
     user = User.find(params[:id])
     if user == current_user
@@ -19,8 +20,7 @@ class UsersController < ApplicationController
       @state = :unauthorized
     end
   end
-
-
+  # 作成
   def create
     user = User.new(user_params)
     if user_ability_params == 1
@@ -30,13 +30,13 @@ class UsersController < ApplicationController
     end
     @state = user.save
   end
-
+  # 削除
   def destroy
     user = User.find(params[:id])
     if user == current_user
-      user.destroy
-    elsif can? :destroy, User
-      user.destroy
+      @state = user.destroy
+    elsif can? :destroy, current_user
+      @state = user.destroy
     else
       @state = :unauthorized
     end
