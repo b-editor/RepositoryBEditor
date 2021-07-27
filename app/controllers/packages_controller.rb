d4372a760ca86f64974c503978e05397fbda92e2
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-07-27 15:07:50 +0900

    [add] add feature of create user

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,8 +3,22 @@ class PackagesController < ApplicationController
   def index
     @packages = Package.all
   end
+  #一件表示
   def show
     @package = Package.find(params[:id])
   end
+  #作成
+  def create
+    package = Package.new(package_params)
+    #current_user(application_controller.rbに定義)で現在ログイン中のUserをプラグイン作成者として登録
+    package.user = current_user
+    #saveメソッドの返り値が結果なのでそれをそのままViewに渡す
+    @state = package.save
+  end
 
+  private
+  #ストロングパラメータ
+  def package_params
+    params.permit(:name,:main_assembly,:homepage,:description,:description_short,:tags,:uuid,:license)
+  end
 end
