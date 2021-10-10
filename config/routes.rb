c8e81ba8a759ef61980400550c431155f69ae278
YiB-PC <hiropcsuki@yahoo.co.jp>
2021-10-10 12:32:55 +0900

    [add] Indigo_sanのWebAPI.mdに従ってRESTに準拠しないルーティングに対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # if you want to use restful routing, use this!
   resources :users
   resources :auth, :only => [ :create, :update,:destroy ]
   resources :packages do
     resources :versions
     end
+  post 'signin', to: 'auth#create'
+  post 'signup', to: 'users#create'
+  get 'getAccountInfo', to: 'users#me'
+  post 'update', to: 'users#updateme'
+  post 'deleteAccount', to: 'users#deleteme'
 end
